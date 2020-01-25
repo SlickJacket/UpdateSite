46c63bfd9bcdc74197bdffb5349f2abdf6926754
SlickJacket <novenarii@gmail.com>
2020-01-25 18:38:56 -0500

    Updates

---
+++ precache-manifest.ae7364d78a8357904ba72d0e556e0cd1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f0a6b3b3aea85c15cd05ddf49592cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1709e896a4c4400bcc58",
+    "url": "/static/css/main.afdf5e98.chunk.css"
+  },
+  {
+    "revision": "5c5ef4e36f8830fb4b10",
+    "url": "/static/js/2.8860f108.chunk.js"
+  },
+  {
+    "revision": "b5c8281ca89c1ddb89c15ead0e00f32f",
+    "url": "/static/js/2.8860f108.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1709e896a4c4400bcc58",
+    "url": "/static/js/main.32a0540c.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "6ec69346350ff0f010eb603062bc6491",
+    "url": "/static/media/Gobold-High-Bold.6ec69346.otf"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/static/media/Lobster-Regular.9406d0ab.ttf"
+  },
+  {
+    "revision": "c46b3b0143d83a675339efd8181dacfd",
+    "url": "/static/media/Melius1.c46b3b01.png"
+  },
+  {
+    "revision": "f51de274a3c8b70fd8e3df868ff18e9e",
+    "url": "/static/media/Melius1gf.f51de274.gif"
+  },
+  {
+    "revision": "01ea8598855576b127e59549b4e49b99",
+    "url": "/static/media/Melius_Logo2.01ea8598.png"
+  },
+  {
+    "revision": "efebca1bd407779e66a44787f9aefd82",
+    "url": "/static/media/MyNewSiteGF.efebca1b.gif"
+  },
+  {
+    "revision": "779f75d6476604fdd29bc01248d00698",
+    "url": "/static/media/MySiteLogo.779f75d6.png"
+  },
+  {
+    "revision": "077e87c6c30f988a8cd225faa83daf18",
+    "url": "/static/media/Oswald-Regular.077e87c6.ttf"
+  },
+  {
+    "revision": "4ed4fcd7cb8bd7d37490a1d383ded66e",
+    "url": "/static/media/Resume.4ed4fcd7.docx"
+  },
+  {
+    "revision": "9d7894bb978db34b1cea5dcd0b0793f2",
+    "url": "/static/media/america-american-flag-architecture-bridge-450597.9d7894bb.jpg"
+  },
+  {
+    "revision": "c0642ff2212a35b0c580ce11bd860012",
+    "url": "/static/media/goldteethusalogo.c0642ff2.png"
+  },
+  {
+    "revision": "ba5ea3f19a5fd1b994672f203aeb24d2",
+    "url": "/static/media/gtusagf.ba5ea3f1.gif"
+  },
+  {
+    "revision": "67ab1d6e8612e97f71602fbe965185e5",
+    "url": "/static/media/melius2gf.67ab1d6e.gif"
+  },
+  {
+    "revision": "0a7471f970927e35afbd7682d276f343",
+    "url": "/static/media/profile.0a7471f9.png"
+  }
+]);
